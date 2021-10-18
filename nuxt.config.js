d11255c86fb29b3c0c112cab208c7802f833c8c2
rezanikafarin <rezanikafarin100@gmail.com>
2021-10-18 18:17:17 +0330

    test sass

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/fonts/Vazir.css',
+    '~/assets/css/style.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
